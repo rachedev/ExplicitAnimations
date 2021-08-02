a3b6cf8ff2319f920adec0f474d563c05057d776
Rachelle Lee <rachedeveloper@gmail.com>
2021-08-01 20:59:20 -0700

    Initial Commit

---
+++ ExplicitAnimations/ExplicitAnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExplicitAnimationsApp.swift
+//  ExplicitAnimations
+//
+//  Created by RachelleLee on 8/1/21.
+//
+
+import SwiftUI
+
+@main
+struct ExplicitAnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
